696d3ebc45177c7e37259840dd603d36b0c51965
Abdeali <abdealichharchhoda786@gmail.com>
2023-03-04 01:29:20 +0530

    New Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
+        Main m = new Main();
+
+        m.Display();
+
         System.out.println("Hello world!");
     }
+
+    void Display() {
+        System.out.println("Hello Abdeali!!");
+    }
+
 }
