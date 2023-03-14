b8f4cf37b17c10788f29b72f390b1b460027da67
Abdeali <abdealichharchhoda786@gmail.com>
2023-03-15 01:08:55 +0530

    Added Form Fields (Image Excluded)

--- src/Frontend/BookStore.java
+++ src/Frontend/BookStore.java
@@ -5,6 +5,7 @@ package Frontend;
 import javax.swing.*;
 import java.awt.*;
 
+/* It is a Main Frame */
 public class BookStore extends JFrame {
 
     /* Component */
@@ -48,7 +49,7 @@ public class BookStore extends JFrame {
         mainHeading = new JLabel();
         mainHeading.setFont(new Font("Arial Rounded MT Bold", Font.BOLD, 36));
         mainHeading.setText("Admin Panel");
-        mainHeading.setBounds(740, 0, 500, 36);
+        mainHeading.setBounds(740, 20, 500, 36);
 
         mainPanel.add(mainHeading);
 
@@ -56,7 +57,7 @@ public class BookStore extends JFrame {
         addBookPanel=new AddBookPanel(this);
         addBookPanel.setLayout(null);
         addBookPanel.setVisible(true);
-        addBookPanel.setBounds(50,50,1500,500);
+        addBookPanel.setBounds(50,70,1500,500);
         addBookPanel.setBackground(new java.awt.Color(240, 240, 140));
         addBookPanel.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED, java.awt.Color.black, java.awt.Color.darkGray, java.awt.Color.black, java.awt.Color.darkGray));
         addBookPanel.setForeground(new java.awt.Color(25, 0, 0));
@@ -67,15 +68,19 @@ public class BookStore extends JFrame {
     }
 }
 
+/* It is a Form (Operation)  Frame */
 class AddBookPanel extends JPanel {
 
     /* Classes */
     BookStore mainContainer;
 
       /* Component */
-      public  JTextField tfBookID,tfBookName,tfBookSubject,tfAuthorName,tfPublication,tfTotalCost;
-      public JLabel lbBookID,lbBookName,lbBookSubject,lbAuthorName,lbPublication,lbTotalCost;
+      public JLabel lbBookID,lbBookName,lbBookSubject,lbAuthorName,lbPublication,lbDatePublication,lbBookPrice,lbBookQuantity,lbTotalCost;
+      public  JTextField tfBookID,tfBookName,tfBookSubject,tfAuthorName,tfPublication,tfDatePublication,tfTotalCost;
+      public JSpinner spBookPrice,spBookQuantity;
+      SpinnerModel valueOfPrice,valueOfQuantity;
       public JButton btnAdd,btnCancle,btnDelete;
+
 
     public AddBookPanel(BookStore mainContainer) {
         this.mainContainer=mainContainer;
@@ -84,18 +89,130 @@ class AddBookPanel extends JPanel {
 
         /* Input 1 : Book ID */
         lbBookID = new JLabel();
-        lbBookID.setText("ID");
+        lbBookID.setText("Book ID");
         lbBookID.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
-        lbBookID.setBounds(50,10,50,35);
+        lbBookID.setBounds(80,20,100,35);
         this.add(lbBookID);
 
         tfBookID = new JTextField();
         tfBookID.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
-        tfBookID.setBounds(100,10,222,30);
+        tfBookID.setBounds(190,20,320,30);
         tfBookID.setToolTipText("Eg. 101");
         this.add(tfBookID);
 
+        /* Input 2 : Book Name */
+        lbBookName = new JLabel();
+        lbBookName.setText("Book Name");
+        lbBookName.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbBookName.setBounds(580,20,150,35);
+        this.add(lbBookName);
+
+        tfBookName = new JTextField();
+        tfBookName.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfBookName.setBounds(710,20,320,30);
+        tfBookName.setToolTipText("Eg. Modern Operating System");
+        this.add(tfBookName);
+
+        /* Input 3 : Book Subject */
+        lbBookSubject = new JLabel();
+        lbBookSubject.setText("Book Subject");
+        lbBookSubject.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbBookSubject.setBounds(50,90,250,35);
+        this.add(lbBookSubject);
+
+        tfBookSubject = new JTextField();
+        tfBookSubject.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfBookSubject.setBounds(190,90,320,30);
+        tfBookSubject.setToolTipText("Eg. Operating System");
+        this.add(tfBookSubject);
+
+
+        /* Input 4 : Author Name */
+        lbAuthorName = new JLabel();
+        lbAuthorName.setText("Author Name");
+        lbAuthorName.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbAuthorName.setBounds(570,90,150,35);
+        this.add(lbAuthorName);
+
+        tfAuthorName = new JTextField();
+        tfAuthorName.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfAuthorName.setBounds(710,90,320,30);
+        tfAuthorName.setToolTipText("Eg. Andrew Tanenbum");
+        this.add(tfAuthorName);
+
+        /* Input 5 : publication */
+        lbPublication = new JLabel();
+        lbPublication.setText("Publication");
+        lbPublication.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbPublication.setBounds(50,160,250,35);
+        this.add(lbPublication);
+
+        tfPublication = new JTextField();
+        tfPublication.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfPublication.setBounds(190,160,320,30);
+        tfPublication.setToolTipText("Eg. Matrix ");
+        this.add(tfPublication);
+
+        /* Input 6 : Date of Publication */
+        lbDatePublication = new JLabel();
+        lbDatePublication.setText("Date Publication");
+        lbDatePublication.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbDatePublication.setBounds(550,160,150,35);
+        this.add(lbDatePublication);
+
+        tfDatePublication = new JTextField();
+        tfDatePublication.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfDatePublication.setBounds(710,160,320,30);
+        tfDatePublication.setToolTipText("Eg. 30/01/2002");
+        this.add(tfDatePublication);
+
+        /* Input 7 : Price Of Book */
+        lbBookPrice = new JLabel();
+        lbBookPrice.setText("Book Price");
+        lbBookPrice.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbBookPrice.setBounds(50,240,250,35);
+        this.add(lbBookPrice);
+
+        valueOfPrice = new SpinnerNumberModel(200, 200, 1500, 1);
+        spBookPrice = new JSpinner(valueOfPrice);
+        spBookPrice.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        spBookPrice.setBounds(150,240,100,30);
+        this.add(spBookPrice);
+
+        /* Input 8 : Quantity Of Book */
+        lbBookQuantity = new JLabel();
+        lbBookQuantity.setText("Book Quantity");
+        lbBookQuantity.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbBookQuantity.setBounds(280,240,250,35);
+        this.add(lbBookQuantity);
+
+        valueOfQuantity = new SpinnerNumberModel(1, 0, 250, 1);
+        spBookQuantity = new JSpinner(valueOfQuantity);
+        spBookQuantity.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        spBookQuantity.setBounds(420,240,100,30);
+        this.add(spBookQuantity);
+
+        /* Input 8 : Total Cost Of Book ( TotalCost = Price * Quantity) */
+        lbTotalCost = new JLabel();
+        lbTotalCost.setText("Total Cost");
+        lbTotalCost.setFont(new java.awt.Font("Yu Gothic Medium", Font.BOLD, 18));
+        lbTotalCost.setBounds(585,240,250,35);
+        this.add(lbTotalCost);
+
+        tfTotalCost = new JTextField();
+        tfTotalCost.setFont(new java.awt.Font("Trebuchet MS", Font.PLAIN, 18)); // NOI18N
+        tfTotalCost.setBounds(710,240,320,30);
+        tfTotalCost.setEditable(false);
+        this.add(tfTotalCost);
 
 
     }
+}
+
+class operationButtonPanel extends JPanel {
+
+}
+
+class LabelsInBookStore extends JLabel {
+
 }
